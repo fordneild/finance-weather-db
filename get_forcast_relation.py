0458dd877223bbcdb9862c5fb406ac0165cf8956
Ford Neild <hanfordn35@gmail.com>
2020-04-16 14:17:48 -0400

    got forcast datd

--- get_forcast_relation.py
+++ get_forcast_relation.py
@@ -16,17 +16,27 @@ for line_index, line in enumerate(lines):
     if date not in dates:
         dates[date] = len(dates) + 1
 
-file2 = open('city_weather.csv')
+file2 = open('weather2.csv')
 lines = file2.readlines()
 lines.pop(0) #remove head of csv file with attribute names
+cleanedForcastLines = []
 for line_index, line in enumerate(lines):
 
-    line_split = line.split(",")[3:]
+    line_split = line.split(",")[1:-1]
+    line_split.pop(2)
     date = line_split[0][1:-1]
+
     # print(date)
     if date in dates:
-        line_split[0] = dates[date]
-        print(line_split)
+        line_split[0] = str(dates[date])
+        line_split[1] = line_split[1][1:-1]
+        line_split[2]= line_split[2][1:-1]
+        line_split[3]= line_split[3][1:-1]
+        # print(line_split)
+        # print(line_split)
+        cleanedForcastLines.append(",".join(line_split) + '\n')
+
+forcastFile.writelines(cleanedForcastLines)
 # print(dates)
 
 
